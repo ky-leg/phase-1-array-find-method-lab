79265628bd849dfe2b994530fb104c4ca3fe0000
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-04 23:16:31 -0400

    that was a tought one feels good to get it right!

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+let noWin = ({result: "no win"})
+let win = {result: "W"}
+
+function identifyW(object) {
+    return object.result === "W";
+    //returns true or false  
+}
+
+function superbowlWin(array) {
+    const win = (array.find(identifyW));
+    if (typeof win === 'object'){
+    return win.year;
+    }
+}
+
+console.log(superbowlWin(record))
+
 
